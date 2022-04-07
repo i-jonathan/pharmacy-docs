f0b46cb90e78251f67a165ad645816aba2c11f62
Jonathan <farinloyejonathan@gmail.com>
2022-04-07 20:06:24 +0100

    In the beginning, I said: "Let there be docs."

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+func main() {
+	router := mux.NewRouter()
+	redoc := http.FileServer(http.Dir("./redoc"))
+	rapidoc := http.FileServer(http.Dir("./rapidoc"))
+
+	router.PathPrefix("/docs").Handler(http.StripPrefix("/docs/", rapidoc))
+	router.PathPrefix("/docs/redoc").Handler(http.StripPrefix("/docs/redoc", redoc))
+
+	_ = http.ListenAndServe(":7000", router)
+}
