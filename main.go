9e0e3e877618571df1564082afcbba125663a401
Jonathan <farinloyejonathan@gmail.com>
2022-04-07 22:38:03 +0100

    Logout endpoint, post user. Goodnight

--- main.go
+++ main.go
@@ -7,11 +7,12 @@ import (
 
 func main() {
 	router := mux.NewRouter()
-	redoc := http.FileServer(http.Dir("./redoc"))
-	rapidoc := http.FileServer(http.Dir("./rapidoc"))
+	//redoc := http.FileServer(http.Dir("./redoc"))
+	rapidoc := http.FileServer(http.Dir("./docs"))
+	//yml := http.FileServer(http.Dir("./"))
 
-	router.PathPrefix("/docs").Handler(http.StripPrefix("/docs/", rapidoc))
-	router.PathPrefix("/docs/redoc").Handler(http.StripPrefix("/docs/redoc", redoc))
+	router.PathPrefix("/docs/").Handler(http.StripPrefix("/docs/", rapidoc))
+	//router.PathPrefix("/docs/redoc").Handler(http.StripPrefix("/docs/redoc", redoc))
 
 	_ = http.ListenAndServe(":7000", router)
 }
